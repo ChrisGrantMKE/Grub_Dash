355f0a4486836844604d9e3c27a3b5e442bc5852
Chris Grant <chrisgrantmke@gmail.com>
2026-03-26 20:06:20 -0600

    Initial orders controller scaffold

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -3,7 +3,11 @@ const path = require("path");
 // Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
 
-// Use this function to assigh ID's when necessary
+// Use this function to assign IDs when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+const VALID_STATUSES = ["pending", "preparing", "out-for-delivery", "delivered"];
+
+function list(request, response) {
+	response.json({ data: orders });
+}
